0463069476d7ec6cefc39dc1abb45212e9bb6416
sidpan89 <76623407+sidpan89@users.noreply.github.com>
2025-05-13 17:43:00 -0400

    Create User.ts

---
+++ backend/src/models/User.ts
@@ -0,0 +1,23 @@
+// src/models/User.ts
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('Users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ unique: true })
+  username!: string;
+
+  @Column({ unique: true })
+  email!: string;
+
+  @Column()
+  passwordHash!: string;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+}
