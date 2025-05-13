39dce5ca9f47e5c8b05dac64b91b3ca282d08ba3
sidpan89 <76623407+sidpan89@users.noreply.github.com>
2025-05-13 17:37:49 -0400

    Create index.ts

---
+++ backend/src/index.ts
@@ -0,0 +1,11 @@
+// src/index.ts
+import app from './app';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
