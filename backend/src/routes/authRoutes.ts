452df17dfd9502419f8b0f637ca6f73a41d6c6fb
sidpan89 <76623407+sidpan89@users.noreply.github.com>
2025-05-13 17:39:44 -0400

    Create authRoutes.ts

---
+++ backend/src/routes/authRoutes.ts
@@ -0,0 +1,10 @@
+// src/routes/authRoutes.ts
+import { Router } from 'express';
+import { login, register } from '../controllers/authController';
+
+const router = Router();
+
+router.post('/login', login);
+router.post('/register', register);
+
+export default router;
